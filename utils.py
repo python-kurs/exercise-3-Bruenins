ca90b05ebb757b213897a2d4671fd5d915cc0500
Bruenins <44396262+Bruenins@users.noreply.github.com>
2019-05-27 15:42:17 +0200

    Exercise 3 Update

--- utils.py
+++ utils.py
@@ -2,3 +2,14 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+
+def countwords(lst:list): 
+    items = {}
+    for i in lst: 
+        if i in items.keys(): 
+            items[i] += 1
+        else:
+            items[i] = 1
+    return(items) 
+    
+
